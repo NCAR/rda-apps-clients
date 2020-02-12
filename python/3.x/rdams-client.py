8a19cfed8c0e4b6fe30ad8d213d4e78f8bb91e9e
Riley Conroy <rpconroy@ucar.edu>
2020-02-12 10:59:02 -0700

    Added argpase to rdams

--- python/3.x/rdams-client.py
+++ python/3.x/rdams-client.py
@@ -1,37 +1,33 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+"""List dataset metadata, subset data subset requests,
+check on request status.
 
-###################################################################################
-###
-###     Title: rdams-client.py
-###    Author: Doug Schuster, schuster@ucar.edu
-###      Date: 10/24/2013
-###   Purpose: List dataset metadata, subset data subset requests, check on request
-###            status.
-###
-###  SVN File: $HeadURL: https://subversion.ucar.edu/svndss/schuster/rest_client/rdams-client.py $
-###
-###  Usage:
-###   rdams-client.py -get_summary <dsnnn.n>
-###   rdams-client.py -get_metadata <dsnnn.n> <-f>
-###   rdams-client.py -get_param_summary <dsnnn.n> <-f>
-###   rdams-client.py -submit [control_file_name]
-###   rdams-client.py -get_status <RequestIndex> <-proc_status>
-###   rdams-client.py -download [RequestIndex]
-###   rdams-client.py -globus_download [RequestIndex]
-###   rdams-client.py -get_control_file_template <dsnnn.n>
-###   rdams-client.py -help
-####################################################################################
+Usage:
+```
+rdams-client.py -get_summary <dsnnn.n>
+rdams-client.py -get_metadata <dsnnn.n> <-f>
+rdams-client.py -get_param_summary <dsnnn.n> <-f>
+rdams-client.py -submit [control_file_name]
+rdams-client.py -get_status <RequestIndex> <-proc_status>
+rdams-client.py -download [RequestIndex]
+rdams-client.py -globus_download [RequestIndex]
+rdams-client.py -get_control_file_template <dsnnn.n>
+rdams-client.py -help
+```
+"""
+__version__ = '0.2'
+__author__ = 'Doug Schuster (schuster@ucar.edu), Riley Conroy (rpconroy@ucar.edu)'
 
-
+import pdb
 import sys
+import os
 import urllib.request
 import urllib.error
 import urllib.parse
-import os
-import sys
 import getpass
 import http.cookiejar
 import json
+import argparse
 
 def update_progress(progress, outdir):
     """Displays or updates a console progress bar
@@ -144,157 +140,187 @@ def download_files(filelist, directory):
         percentcomplete = (float(filecount) / float(length))
     update_progress(percentcomplete, directory)
 
-sys.tracebacklimit = 0
-base = 'https://rda.ucar.edu/apps/'
-jsondata = ''
-username = ''
-password = ''
-pwfile = './rdamspw.txt'
-pwstring = ''
-npwstring = ''
-controlfile = ''
-controlparms = {}
-cookie_file = 'auth.rda_ucar_edu'
-loginurl = 'https://rda.ucar.edu/cgi-bin/login'
-exitstring = "\nUsage: \nrdams-client.py -get_summary <dsnnn.n>\nrdams-client.py -get_metadata <dsnnn.n>\nrdams-client.py -get_param_summary <dsnnn.n>\nrdams-client.py -submit [control_file_name]\nrdams-client.py -get_status <RequestIndex> <-proc_status>\nrdams-client.py -download [RequestIndex]\nrdams-client.py -globus_download [RequestIndex]\nrdams-client.py -purge [RequestIndex]\nrdams-client.py -get_control_file_template <dsnnn.n>\nrdams-client.py -help\n\n"
+def get_parser():
+    description = "Queries NCAR RDA REST API."
+    parser = argparse.ArgumentParser(prog='rdams', description=description)
+    parser.add_argument('-get_summary', '-g',
+            type=str,
+            metavar='dsid',
+            required=False,
+            help="Get a summary of the given dataset.")
+    parser.add_argument('-get_metadata', '-gm', type=str, required=False, help="Get metadata for a given dataset")
+    parser.add_argument('-get_param_summary', '-gpm', type=str, required=False, help="Get only parameters for a given dataset")
+    parser.add_argument('-submit', '-s', type=str, required=False, help="Submit a request using a control file")
+    parser.add_argument('-get_status', '-gs', type=str, required=False, help="Get a summary of the given dataset.")
+    parser.add_argument('-download', '-d', type=str, required=False, help="Download data given a request id")
+    parser.add_argument('-globus_download', '-gd', type=str, required=False, help="Start a globus transfer for a give request index.")
+    parser.add_argument('-get_control_file_template', '-gt', type=str, required=False, help="Get a template control file used for subsetting")
+    return parser
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == "-get_summary":
-        print('\nGetting summary information.  Please wait as this may take awhile.\n')
-        theurl = base + 'summary'
-        if len(sys.argv) > 2:
-            theurl = base + 'summary/' + add_ds_str(sys.argv[2])
-    elif sys.argv[1] == "-get_metadata":
-        print('\nGetting metadata.  Please wait as this may take awhile.\n')
-        theurl = base + 'metadata'
-        if len(sys.argv) == 3:
-            theurl = base + 'metadata/' + add_ds_str(sys.argv[2])
-        elif len(sys.argv) == 4:
-            theurl = base + 'metadata/' + add_ds_str(sys.argv[2]) + '/formatted'
-    elif sys.argv[1] == "-get_param_summary":
-        print('\nGetting parameter summary.  Please wait as this may take awhile.\n')
-        theurl = base + 'paramsummary'
-        if len(sys.argv) == 3:
-            theurl = base + 'paramsummary/' + add_ds_str(sys.argv[2])
-        elif len(sys.argv) == 4:
-            theurl = base + 'paramsummary/'+ add_ds_str(sys.argv[2]) + '/formatted'
-    elif sys.argv[1] == "-help":
-        theurl = base + 'help'
-    elif sys.argv[1] == "-get_control_file_template":
-        theurl = base + 'template'
-        controlfile = './dsnnn.n_control_file'
-        if len(sys.argv) > 2:
-            theurl = base + 'template/' + add_ds_str(sys.argv[2])
-            controlfile = './' + add_ds_str(sys.argv[2]) + '_control_file'
-    elif sys.argv[1] == "-get_status":
-        theurl = base + 'request'
-        if len(sys.argv) == 3:
-            theurl = base + 'request/' + sys.argv[2]
-        elif len(sys.argv) == 4:
-            theurl = base + 'request/' + sys.argv[2] + '/' + add_ds_str(sys.argv[3])
-    elif sys.argv[1] == "-download":
-        if len(sys.argv) > 2:
-            theurl = base + 'request/' + sys.argv[2] + '/filelist'
-        else:
-            sys.exit("\nUsage: \nrdams-client.py -download [RequestIndex]\n")
-    elif sys.argv[1] == "-globus_download":
-        if len(sys.argv) > 2:
-            theurl = base+'request/' + sys.argv[2] + '/-globus_download'
-        else:
-            sys.exit("\nUsage: \nrdams-client.py -globus_download [RequestIndex]\n")
-    elif sys.argv[1] == "-purge":
-        if len(sys.argv) > 2:
-            theurl = base + 'request/' + sys.argv[2]
-        else:
-            sys.exit("\nUsage: \nrdams-client.py -purge [RequestIndex]\n")
-    elif sys.argv[1] == "-submit":
-        if len(sys.argv) > 2:
+def query(args):
+    """Perform a query based on command line like arguments.
+    """
+    parser = get_parser()
+    if len(args) == 1:
+        parser.parse_args(['-h'])
+        exit(1)
+    args = parser.parse_args(args)
+    pdb.set_trace()
+
+    sys.tracebacklimit = 0
+    base = 'https://rda.ucar.edu/apps/'
+    jsondata = ''
+    username = ''
+    password = ''
+    pwfile = './rdamspw.txt'
+    pwstring = ''
+    npwstring = ''
+    controlfile = ''
+    controlparms = {}
+    cookie_file = 'auth.rda_ucar_edu'
+    loginurl = 'https://rda.ucar.edu/cgi-bin/login'
+    exitstring = "\nUsage: \nrdams-client.py -get_summary <dsnnn.n>\nrdams-client.py -get_metadata <dsnnn.n>\nrdams-client.py -get_param_summary <dsnnn.n>\nrdams-client.py -submit [control_file_name]\nrdams-client.py -get_status <RequestIndex> <-proc_status>\nrdams-client.py -download [RequestIndex]\nrdams-client.py -globus_download [RequestIndex]\nrdams-client.py -purge [RequestIndex]\nrdams-client.py -get_control_file_template <dsnnn.n>\nrdams-client.py -help\n\n"
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-get_summary":
+            print('\nGetting summary information.  Please wait as this may take awhile.\n')
+            theurl = base + 'summary'
+            if len(sys.argv) > 2:
+                theurl = base + 'summary/' + add_ds_str(sys.argv[2])
+        elif sys.argv[1] == "-get_metadata":
+            print('\nGetting metadata.  Please wait as this may take awhile.\n')
+            theurl = base + 'metadata'
+            if len(sys.argv) == 3:
+                theurl = base + 'metadata/' + add_ds_str(sys.argv[2])
+            elif len(sys.argv) == 4:
+                theurl = base + 'metadata/' + add_ds_str(sys.argv[2]) + '/formatted'
+        elif sys.argv[1] == "-get_param_summary":
+            print('\nGetting parameter summary.  Please wait as this may take awhile.\n')
+            theurl = base + 'paramsummary'
+            if len(sys.argv) == 3:
+                theurl = base + 'paramsummary/' + add_ds_str(sys.argv[2])
+            elif len(sys.argv) == 4:
+                theurl = base + 'paramsummary/'+ add_ds_str(sys.argv[2]) + '/formatted'
+        elif sys.argv[1] == "-help":
+            theurl = base + 'help'
+        elif sys.argv[1] == "-get_control_file_template":
+            theurl = base + 'template'
+            controlfile = './dsnnn.n_control_file'
+            if len(sys.argv) > 2:
+                theurl = base + 'template/' + add_ds_str(sys.argv[2])
+                controlfile = './' + add_ds_str(sys.argv[2]) + '_control_file'
+        elif sys.argv[1] == "-get_status":
             theurl = base + 'request'
-            with open(sys.argv[2], "r") as myfile:
-                for line in myfile:
-                    if line.startswith('#'):
-                        continue
-                    li = line.rstrip()
-                    (key, value) = li.split('=', 2)
-                    controlparms[key] = value
-            jsondata = '{'
-            for k in list(controlparms.keys()):
-                jsondata += '"' + k + '"' + ":" + '"' + controlparms[k] + '",'
-            jsondata = jsondata[:-1]
-            jsondata += '}'
-            print('\nSubmitting request.  Please wait as this may take awhile.\n')
+            if len(sys.argv) == 3:
+                theurl = base + 'request/' + sys.argv[2]
+            elif len(sys.argv) == 4:
+                theurl = base + 'request/' + sys.argv[2] + '/' + add_ds_str(sys.argv[3])
+        elif sys.argv[1] == "-download":
+            if len(sys.argv) > 2:
+                theurl = base + 'request/' + sys.argv[2] + '/filelist'
+            else:
+                sys.exit("\nUsage: \nrdams-client.py -download [RequestIndex]\n")
+        elif sys.argv[1] == "-globus_download":
+            if len(sys.argv) > 2:
+                theurl = base+'request/' + sys.argv[2] + '/-globus_download'
+            else:
+                sys.exit("\nUsage: \nrdams-client.py -globus_download [RequestIndex]\n")
+        elif sys.argv[1] == "-purge":
+            if len(sys.argv) > 2:
+                theurl = base + 'request/' + sys.argv[2]
+            else:
+                sys.exit("\nUsage: \nrdams-client.py -purge [RequestIndex]\n")
+        elif sys.argv[1] == "-submit":
+            if len(sys.argv) > 2:
+                theurl = base + 'request'
+                with open(sys.argv[2], "r") as myfile:
+                    for line in myfile:
+                        if line.startswith('#'):
+                            continue
+                        li = line.rstrip()
+                        (key, value) = li.split('=', 2)
+                        controlparms[key] = value
+                jsondata = '{'
+                for k in list(controlparms.keys()):
+                    jsondata += '"' + k + '"' + ":" + '"' + controlparms[k] + '",'
+                jsondata = jsondata[:-1]
+                jsondata += '}'
+                print('\nSubmitting request.  Please wait as this may take awhile.\n')
+            else:
+                sys.exit(
+                    "\nUsage: \nrdams-clientpy -submit [control_file_name]\n")
         else:
-            sys.exit(
-                "\nUsage: \nrdams-clientpy -submit [control_file_name]\n")
+            sys.exit(exitstring)
     else:
         sys.exit(exitstring)
-else:
-    sys.exit(exitstring)
 
 
-if os.path.isfile(pwfile) and os.path.getsize(pwfile) > 0:
-    (username, password) = read_pw_file(pwfile)
-else:
-    (username, password) = get_userinfo()
-
-opener = add_http_auth(theurl, username, password)
-
-if len(jsondata) > 1:
-    request = urllib.request.Request(
-        theurl, jsondata.encode(), {'Content-type': 'application/json'})
-else:
-    request = urllib.request.Request(theurl)
-
-if sys.argv[1] == "-purge":
-    request.get_method = lambda: 'DELETE'
-
-try:
-    url = opener.open(request)
-except urllib.error.HTTPError as e:
-    if e.code == 401:
-        print('RDA username and password invalid.  Please try again\n')
+    if os.path.isfile(pwfile) and os.path.getsize(pwfile) > 0:
+        (username, password) = read_pw_file(pwfile)
+    else:
         (username, password) = get_userinfo()
-        opener = add_http_auth(theurl, username, password)
-        try:
-            url = opener.open(request)
-        except urllib.error.HTTPError as e:
-            if e.code == 401:
-                print(
-                    'RDA username and password invalid, or you are not authorized to access this dataset.\n')
-                print('Please verify your login information at http://rda.ucar.edu\n.')
-                sys.exit()
+    opener = add_http_auth(theurl, username, password)
+
+    if len(jsondata) > 1:
+        request = urllib.request.Request(
+            theurl, jsondata.encode(), {'Content-type': 'application/json'})
+    else:
+        request = urllib.request.Request(theurl)
+
+    if sys.argv[1] == "-purge":
+        request.get_method = lambda: 'DELETE'
+
+    try:
+        url = opener.open(request)
+    except urllib.error.HTTPError as e:
+        if e.code == 401:
+            print('RDA username and password invalid.  Please try again\n')
+            (username, password) = get_userinfo()
+            opener = add_http_auth(theurl, username, password)
+            try:
+                url = opener.open(request)
+            except urllib.error.HTTPError as e:
+                if e.code == 401:
+                    print(
+                        'RDA username and password invalid, or you are not authorized to access this dataset.\n')
+                    print('Please verify your login information at http://rda.ucar.edu\n.')
+                    sys.exit()
 
 
-write_pw_file(pwfile, username, password)
+    write_pw_file(pwfile, username, password)
 
 
-if sys.argv[1] == "-get_control_file_template":
-    print('\nWriting example control file to ' + controlfile + '\n')
-    with open(controlfile, "wb") as fo:
-        fo.write(url.read())
-    sys.exit()
-if sys.argv[1] == "-download":
-    authdata = 'email=' + username + '&password=' + password + '&action=login'
-    authdata = authdata.encode()
+    if sys.argv[1] == "-get_control_file_template":
+        print('\nWriting example control file to ' + controlfile + '\n')
+        with open(controlfile, "wb") as fo:
+            fo.write(url.read())
+        sys.exit()
+    if sys.argv[1] == "-download":
+        authdata = 'email=' + username + '&password=' + password + '&action=login'
+        authdata = authdata.encode()
 
-    jsonfilelist = url.read().decode()
+        jsonfilelist = url.read().decode()
 
-    if jsonfilelist[0] != "{":
-        print(jsonfilelist)
+        if jsonfilelist[0] != "{":
+            print(jsonfilelist)
+            sys.exit()
+
+        filelist = json.loads(jsonfilelist)
+        length = len(filelist)
+
+        directory = 'rda_request_' + sys.argv[2]
+
+        # get cookie required to download data files
+        add_http_cookie(loginurl, authdata)
+
+        print("\n\nStarting Download.\n\n")
+
+        download_files(filelist, directory)
+
         sys.exit()
 
-    filelist = json.loads(jsonfilelist)
-    length = len(filelist)
+    print(url.read().decode())
 
-    directory = 'rda_request_' + sys.argv[2]
-
-    # get cookie required to download data files
-    add_http_cookie(loginurl, authdata)
-
-    print("\n\nStarting Download.\n\n")
-
-    download_files(filelist, directory)
-
-    sys.exit()
-
-print(url.read().decode())
+if __name__ == "__main__":
+    """Calls Generic main method"""
+    query(sys.argv)
